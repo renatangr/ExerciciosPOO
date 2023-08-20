7997d47cc2192b9bfb6fab229df38bb7c361f5ee
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-08-20 17:27:56 -0300

    Criado o objeto Pessoa.java

---
+++ Aula01/src/exercicio01/Pessoa.java
@@ -0,0 +1,41 @@
+package exercicio01;
+
+public class Pessoa {
+    private String CPF;
+    private String nome;
+    private char sexo;
+    private int idade;
+
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
