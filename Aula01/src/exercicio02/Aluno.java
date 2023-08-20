8fad30045e8edcd76e3f8113a7e35f956aaea51e
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-08-20 18:19:06 -0300

    Criado objeto aluno

---
+++ Aula01/src/exercicio02/Aluno.java
@@ -0,0 +1,61 @@
+
+package exercicio02;
+
+public class Aluno {
+    private String nome;
+    private String ra;
+    private double ac1;
+    private double ac2;
+    private double ag;
+    private double af;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public double getAc1() {
+        return ac1;
+    }
+
+    public void setAc1(double ac1) {
+        this.ac1 = ac1;
+    }
+
+    public double getAc2() {
+        return ac2;
+    }
+
+    public void setAc2(double ac2) {
+        this.ac2 = ac2;
+    }
+
+    public double getAg() {
+        return ag;
+    }
+
+    public void setAg(double ag) {
+        this.ag = ag;
+    }
+
+    public double getAf() {
+        return af;
+    }
+
+    public void setAf(double af) {
+        this.af = af;
+    }
+    
+    
+}
