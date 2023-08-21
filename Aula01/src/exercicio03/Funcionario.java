b7a6780eca70453395364e720dbf13027c8063e7
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-08-21 07:13:35 -0300

    Create Funcionario.java

---
+++ Aula01/src/exercicio03/Funcionario.java
@@ -0,0 +1,98 @@
+
+package exercicio03;
+
+public class Funcionario {
+    private String nome;
+    private int cracha;
+    private int tipoVinculo;
+    private double valorHora;
+    private double qtdeHora;
+    private double salario;
+    private double valorDesconto;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getCracha() {
+        return cracha;
+    }
+
+    public void setCracha(int cracha) {
+        this.cracha = cracha;
+    }
+
+    public int getTipoVinculo() {
+        return tipoVinculo;
+    }
+
+    public void setTipoVinculo(int tipoVinculo) {
+        this.tipoVinculo = tipoVinculo;
+    }
+
+    public double getValorHora() {
+        return valorHora;
+    }
+
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
+
+    public double getQtdeHora() {
+        return qtdeHora;
+    }
+
+    public void setQtdeHora(double qtdeHora) {
+        this.qtdeHora = qtdeHora;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+    public double getValorDesconto() {
+        return valorDesconto;
+    }
+
+    public void setValorDesconto(double valorDesconto) {
+        this.valorDesconto = valorDesconto;
+    }
+    
+    public double calculaSalario () {
+        if (tipoVinculo == 0) {
+            double salarioHora = valorHora * qtdeHora;
+            return salarioHora;
+        } else {
+            return salario;
+        }
+    }
+    
+    public double calculaValorReceber () {
+        if (tipoVinculo == 0) {
+            double descontadoHora = (valorHora * qtdeHora) - valorDesconto;
+            return descontadoHora;
+        } else {
+            double descontado = salario - valorDesconto;
+            return descontado;
+        }
+    }
+    
+    public String imprimir () {
+        String str = "Nome: " + getNome() +
+                     "\nCrachá: " + getCracha() +
+                     "\nVínculo: " + getTipoVinculo() +
+                     "\nSalário: " + calculaSalario() +
+                     "\nDesconto: " + getValorDesconto() +
+                     "\nValor a receber: " + calculaValorReceber();
+        return str;
+    }
+}
+
