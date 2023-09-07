8ca0f691f9c14266b96c086cccaaeca40bd2df9d
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-07 12:53:25 -0300

    Update Vendedor.java

--- Aula02/src/exercicio02/Vendedor.java
+++ Aula02/src/exercicio02/Vendedor.java
@@ -1,9 +1,35 @@
 package exercicio02;
 
 public class Vendedor {
+    private int codigo;
+    private String nome;
+    private double percentualComissao;
 
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public int getCodigo() {
+        return codigo;
     }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPercentualComissao() {
+        return percentualComissao;
+    }
+
+    public void setPercentualComissao(double percentualComissao) {
+        this.percentualComissao = percentualComissao;
+    }
+    
+    
+    
     
 }
