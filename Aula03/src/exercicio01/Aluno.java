c7f2bd3293353090ca78450aa20d0732b9afe691
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-17 17:45:57 -0300

    Update Aluno.java

--- Aula03/src/exercicio01/Aluno.java
+++ Aula03/src/exercicio01/Aluno.java
@@ -1,13 +1,40 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package exercicio01;
 
-/**
- *
- * @author RenataMorinigoLimaNe
- */
 public class Aluno {
+    private String ra;
+    private String nome;
+
+    public Aluno () {
+        this.nome = nome;
+        this.ra = ra;
+    }
+    
+    public Aluno (String ra, String nome) {
+        this.nome = nome;
+        this.ra = ra;
+        
+    }
+    
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String imprimir () {
+        return  "\n====================" +
+                "\nRA: " +ra+
+                "\nNome: " +nome;         
+    }
     
 }
