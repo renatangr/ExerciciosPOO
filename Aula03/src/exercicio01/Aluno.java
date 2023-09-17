6c1355bd9bf61039c665e6dc7f81f9f87dc3ef4d
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-17 17:43:58 -0300

    Create Aluno.java

---
+++ Aula03/src/exercicio01/Aluno.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package exercicio01;
+
+/**
+ *
+ * @author RenataMorinigoLimaNe
+ */
+public class Aluno {
+    
+}
---
+++ Aula03/src/exercicio01/MainExercicio01.java
@@ -0,0 +1,9 @@
+package exercicio01;
+
+public class MainExercicio01 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
