4076a6ee4d884a309e679f417e8219133fc75557
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-29 20:18:20 -0300

    Create Aluno.java

---
+++ Aula04/src/exercicio01/Aluno.java
@@ -0,0 +1,34 @@
+
+package exercicio01;
+
+public class Aluno extends Pessoa {
+    private String ra;
+    private double nota1;
+    private double nota2;
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public double getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public double getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+   
+    
+}
