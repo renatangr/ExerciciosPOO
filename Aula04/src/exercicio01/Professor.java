16de4da16c07f20e4b449c798b88eaca82a61ee3
Renata Negrini | Estudante de ADS | 4º Sem <renatanegrinim@gmail.com>
2023-09-29 20:18:28 -0300

    Create Professor.java

---
+++ Aula04/src/exercicio01/Professor.java
@@ -0,0 +1,42 @@
+package exercicio01;
+
+public class Professor extends Pessoa {
+    
+    private String cracha;
+    private char tipoVinculo;
+    private double salario;
+
+    public Professor(String nome, String cpf, String endereco) {
+        super(nome, cpf, endereco);
+        this.cracha = cracha;
+        this.tipoVinculo = tipoVinculo;
+        this.salario = salario;
+    }
+
+    
+    public String getCracha() {
+        return cracha;
+    }
+
+    public void setCracha(String cracha) {
+        this.cracha = cracha;
+    }
+
+    public char getTipoVinculo() {
+        return tipoVinculo;
+    }
+
+    public void setTipoVinculo(char tipoVinculo) {
+        this.tipoVinculo = tipoVinculo;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    
+    
+}
